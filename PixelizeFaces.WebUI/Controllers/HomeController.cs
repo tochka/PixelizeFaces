ca6fa405d2cb94249b665a3b07ea80aea96872f3
Tochka <vinogradovve@gmail.com>
2013-08-24 17:32:22 +0600

    wrtie english text

---
+++ PixelizeFaces.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FaceDetection;
+using PixelizeFaces.Domain.Abstract;
+using PixelizeFaces.Domain.Entities;
+
+namespace PixelizeFaces.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DetectFace _detectFace;
+        private readonly IFiltering _filters;
+        private readonly IPictureRepository _repository;
+
+        public HomeController(DetectFace detectFace, IFiltering fiters, IPictureRepository repository)
+        {
+            _detectFace = detectFace;
+            _filters = fiters;
+            _repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {            
+            return View();
+        }
+
+        public FileContentResult GetImage(int pictureId = 0, bool original = true)
+        {
+            var picture = _repository.Picture.FirstOrDefault(item => item.PictureId == pictureId);
+            if (picture == null)
+                return null;
+
+            return File(original ? picture.BeforePicture : picture.AfterPicture, picture.ImageMimeType);
+        }
+
+        [HttpPost]
+        public RedirectToRouteResult AddPicture(HttpPostedFileBase image)
+        {
+            if (image == null)
+                return RedirectToAction("Index");
+            
+            var picture = new Picture
+                              {
+                                  ImageMimeType = image.ContentType,
+                                  BeforePicture = new byte[image.ContentLength],
+                                  AfterPicture = new byte[image.ContentLength],
+                                  Name = image.FileName
+                              };
+
+            image.InputStream.Read(picture.BeforePicture, 0, image.ContentLength);
+
+            var pictureAfter = new Bitmap(image.InputStream);
+            var rects = _detectFace.Detect(pictureAfter);
+            _filters.Apply(pictureAfter, rects);
+
+            var converter = new ImageConverter();
+            picture.AfterPicture = (byte[]) converter.ConvertTo(pictureAfter, typeof (byte[]));            
+
+            _repository.SavePicture(picture);
+            return RedirectToAction("ViewPictures", new { pictureId = picture.PictureId });
+        }
+
+        public ActionResult ViewPictures(int pictureId = 0)
+        {
+            var picture = _repository.Picture.FirstOrDefault(item => item.PictureId == pictureId);
+            if (picture == null)
+                return null;
+            return View(picture);
+        }
+
+        public FileStreamResult Download(int pictureId = 0)
+        {
+            var picture = _repository.Picture
+                .FirstOrDefault(item => item.PictureId == pictureId);
+            if (picture == null)
+                return null;
+
+            var stream = new MemoryStream(picture.AfterPicture);
+            return File(stream, picture.ImageMimeType, picture.Name);
+        }
+    }
+}
---
+++ PixelizeFaces.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FaceDetection;
+using Ninject;
+using System.Web.Mvc;
+using Ninject.Parameters;
+using PixelizeFaces.Domain.Abstract;
+using PixelizeFaces.Domain.Concrete;
+
+namespace PixelizeFaces.WebUI.Infrastructure
+
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private readonly IKernel _ninjectKernel;
+
+        public NinjectControllerFactory(string serverPath)
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBindings(serverPath);
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null
+                ? null
+                : (IController)_ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings(string serverPath)
+        {
+            _ninjectKernel.Bind<IPictureRepository>().To<EFPictureRepository>();
+
+            // TODO Использовать относительный путь            
+            var faceDetect = new DetectFace(serverPath+"bin\\haarcascade_frontalface_default.xml");
+            _ninjectKernel.Bind<DetectFace>().ToConstant(faceDetect);
+            _ninjectKernel.Bind<IFiltering>().To<Pixelate>()
+                .WithConstructorArgument("pixelsX",7)
+                .WithConstructorArgument("pixelsY",7);                
+        }
+    }
+}
